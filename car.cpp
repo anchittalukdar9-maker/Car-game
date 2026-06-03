04a688b1e6e97217faadfde0271922f1ce95a357
Anchit Talukdar <anchittalukdar9@gmail.com>
2026-06-04 00:13:56 +0530

    added car steering

--- car.cpp
+++ car.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 int length=10;
 int width = 20;
 //car positions acc to width and lanes are 2,6,10,14,18 but start from 1,5,9,13,17
+int dir = 0;
 int car=9;
 //drawing the lanes and the car
 void draw(){
@@ -28,5 +30,18 @@ void draw(){
     }
 }
 int main(){
+    while(true){
+    system("cls");
     draw();
+    dir = 0;
+    cout<<"Enter 1 to move left and 2 to move right: ";
+    cin>>dir;
+    if(dir==1 && car>1){
+        car-=4;
+    }else if(dir==2 && car<17){
+        car+=4;
+    }
+
+    }
+    return 0;
 }
